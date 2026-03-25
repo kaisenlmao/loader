b6ac250eecec523f47f4da0233e3b0f522886bff
kaisenlmao <marvnesterking@gmail.com>
2026-03-25 17:23:52 +0800

    Increase Toggle Button Icon Size from 34 to 41

--- LibraryV3.lua
+++ LibraryV3.lua
@@ -8393,7 +8393,7 @@ do
         local ToggleBtnTexture = "rbxassetid://72530843154458"
         
         local ToggleBtnSize = 56   
-        local ToggleBtnIconSize = 34 
+        local ToggleBtnIconSize = 41 
 
         local ToggleBtnFrame = New("ImageButton", {
             AnchorPoint = Vector2.new(0.5, 0),
